e81889e0a4e53719f65d3427e42334bcc9bf23d4
Ethan Harlig <ethan.harlig@gmail.com>
2018-03-12 14:23:27 -0700

    Posting time comments

--- src/main/scala/PostingTimeComments.scala
+++ src/main/scala/PostingTimeComments.scala
@@ -1,3 +1,43 @@
+import java.util.Date
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+
 object PostingTimeComments {
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    val conf = new SparkConf().setAppName("Driver").setMaster("local[4]")
+
+    val sc = new SparkContext(conf)
+
+    val lines = sc.textFile("data/reddit.csv")
+
+    var minComments = Integer.MAX_VALUE
+    var maxComments = 0
+
+    lines.map(
+      line =>
+        line.split(",").map(
+        s =>
+          s.trim()
+      )
+    ).map(
+      spl =>
+        (Integer.parseInt(spl(4)), Integer.parseInt(spl(5)))
+    ).map{ case (com, time) =>
+
+        if (com > maxComments) {
+          maxComments = com
+        }
+        if (com < minComments) {
+          minComments = com
+        }
+
+//      println(new Date(time))
+      (com, time)
+    }.foreach(println)
+  }
 
 }
