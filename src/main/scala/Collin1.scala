8eb7d4294f6bce31ca17916dc2229b988fa16b6d
Collin Dutter <cdutter@calpoly.edu>
2018-03-12 14:30:27 -0700

    Added util class

--- src/main/scala/Collin1.scala
+++ src/main/scala/Collin1.scala
@@ -1,3 +1,13 @@
-object Collin1 {
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkConf
 
+object Collin1 {
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    val conf = new SparkConf().setAppName("Collin1").setMaster("local[4]")
+
+    RedditUtil.getRedditRDD(conf)
+  }
 }
