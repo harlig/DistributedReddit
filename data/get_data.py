c7885431a513aaf033cb55b7fd20400aaef4037b
Ethan Harlig <ethan.harlig@gmail.com>
2018-03-12 14:45:12 -0700

    Working script to get data

--- data/get_data.py
+++ data/get_data.py
@@ -22,7 +22,7 @@ for ndx, sub in enumerate(TOP_SUBS):
     for i, submission in enumerate(reddit.subreddit(sub).top(limit=1000)):
         votes = submission.score
         sub = submission.subreddit
-        title = submission.title.replace(',', '').replace('"', '')
+        title = submission.title.replace(',', '').replace('"', '').strip()
         time_created = submission.created
         num_comments = submission.num_comments
 
